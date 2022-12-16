f5abca7cd0bab3c85bb98ef04be847fbcbbf0c5e
joekern2 <116761823+joekern2@users.noreply.github.com>
2022-12-16 15:00:57 -0600

    Add files via upload

--- SalesRunFile.py
+++ SalesRunFile.py
@@ -26,6 +26,12 @@ def run():
                                    'Unnamed: 5', 'Unnamed: 6', 'Unnamed: 7', 'Unnamed: 8', 'Unnamed: 9',
                                    'Unnamed: 10', 'Unnamed: 11', 'Unnamed: 12', 'Unnamed: 13']]
     temp = temp[14:26]
+    temp.rename(columns={'SALESMAN P&L': 'Month', 'Unnamed: 2': 'Goal Units - New', 'Unnamed: 3': 'Goal GM - New', 
+                         'Unnamed: 4': 'Actual Units - New', 'Unnamed: 5': 'Actual GM - New',
+                         'Unnamed: 6': 'Goal Units - Used', 'Unnamed: 7': 'Goal GM - Used',
+                         'Unnamed: 8': 'Actual Units - Used', 'Unnamed: 9': 'Actual GM - Used',
+                         'Unnamed: 10': 'Goal Units - Total', 'Unnamed: 11': 'Goal GM - Total',
+                         'Unnamed: 12': 'Actual Units - Total', 'Unnamed: 13': 'Actual GM - Total'}, inplace=True)
     st.dataframe(temp)
 
     if mc == 1:
